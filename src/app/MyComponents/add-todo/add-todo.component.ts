dee7a95a09b0d21a4d0ea856dc1b20c8a8486a02
AswinLokzz <helloaswinx1x@gmail.com>
2023-10-09 10:35:57 +0530

    hello

---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,25 @@
+import { Component,Output,EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent {
+  title: string;
+  description: string;
+
+  @Output()
+  todoAdd: EventEmitter<Todo>= new EventEmitter()
+
+  onSubmit(){
+    const todo={
+      sno: 8,
+      title: this.title,
+      description: this.description,
+      active: true
+    }
+    this.todoAdd.emit(todo)
+  }
+}
